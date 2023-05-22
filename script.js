78dfef7fc173eaa984a024543516f30e89929c65
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-05-22 19:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let p = document.getElementById("status");
+
+function change() {
+	let h = document.createElement("h1");
+	h.innerText = "Entered Metaverse";
+	p.innerText = "";
+	p.appendChild(h);
+}
